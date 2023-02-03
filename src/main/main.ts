ca2917b6c1f9fc6cbead3da538c8fd49191b4aff
Cornelius Grunwald <cornelius.grunwald@tu-dortmund.de>
2023-02-03 11:33:43 +0100

    add images

--- src/main/main.ts
+++ src/main/main.ts
@@ -77,6 +77,7 @@ const createWindow = async () => {
     autoHideMenuBar: true,
     webPreferences: {
       webSecurity: false,
+      devTools: false,
       preload: app.isPackaged
         ? path.join(__dirname, 'preload.js')
         : path.join(__dirname, '../../.erb/dll/preload.js'),
